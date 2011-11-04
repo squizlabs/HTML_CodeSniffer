c31c918ef9571002cfa24052f7cdb22f56022bb9
Luke Wright <lwright@squiz.com.au>
2011-11-04 19:31:00 +1100

    Added SC 2.4.1 (bypass blocks) sniff, with test for using title attributes on iframe elements (H64).
    H64 also refers to frame elements in general, but HTMLCodeSniffer won't be particularly useful on the few documents using frameset as it is (as opposed to the individual frames). So support on iframe only for now, which is still relevant.

---
+++ Standards/WCAG2AAA/Sniffs/Principle2/Guideline2_4/2_4_1.js
@@ -0,0 +1,27 @@
+var HTMLCS_WCAG2AAA_Sniffs_Principle2_Guideline2_4_2_4_1 = {
+    register: function()
+    {
+        return ['iframe'];
+
+    },
+
+    process: function(element)
+    {
+        var nodeName = element.nodeName.toLowerCase();
+
+        if (nodeName === 'iframe') {
+            var hasTitle = false;
+            if (element.hasAttribute('title') === true) {
+                if (element.getAttribute('title') && (/^\s+$/.test(element.getAttribute('title')) === false)) {
+                    hasTitle = true;
+                }
+            }
+
+            if (hasTitle === false) {
+                HTMLCS.addMessage(HTMLCS.ERROR, element, 'Check this element for the presence of a title attribute that identifies the frame.', 'H64.1');
+            } else
+                HTMLCS.addMessage(HTMLCS.NOTICE, element, 'Check that the title attribute of this element contains text that identifies the frame.', 'H64.2');
+            }
+        }
+    }
+};
