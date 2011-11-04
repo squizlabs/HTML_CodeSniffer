8405afed1b6a6632fbc90fad737e52345d70c575
Luke Wright <lwright@squiz.com.au>
2011-11-04 14:51:44 +1100

    Recategorise the meta refresh error as failure F41, rather than technique H76.
    H76 only covers client-side redirects using meta refresh, but F41 also covers automatic refreshes using meta refresh but with no URL (eg. <meta http-equiv="refresh" content="60" />). This is considered "non-adjustable" to the user and thus fails 2.2.1.

--- Standards/WCAG2AAA/Sniffs/Principle2/Guideline2_2/2_2_1.js
+++ Standards/WCAG2AAA/Sniffs/Principle2/Guideline2_2/2_2_1.js
@@ -7,14 +7,14 @@ var HTMLCS_WCAG2AAA_Sniffs_Principle2_Guideline2_2_2_2_1 = {
 
     process: function(element)
     {
-        // Meta refresh testing under H76. Fails if a non-zero timeout is provided.
-        // NOTE: this technique lists only 3.2.5, however the related failure -
-        // F41 - also lists 2.2.1 (as meta refresh times are not adjustable).
-        // Hence this is important at A level and not just AAA.
+        // Meta refresh testing under H76/F41. Fails if a non-zero timeout is provided.
+        // NOTE: H76 only lists criterion 3.2.5, but F41 also covers refreshes to
+        // same page (no URL content), which is covered by non-adjustable timeouts
+        // in criterion 2.2.1.
         if (element.hasAttribute('http-equiv') === true) {
             if ((String(element.getAttribute('http-equiv'))).toLowerCase() === 'refresh')) {
-                if (/^[1-9]\d*;\s+url=/.test(element.getAttribute('content').toLowerCase()) === false) {
-                    HTMLCS.addMessage(HTMLCS.ERROR, element, 'When using a client-side redirect using meta refresh, the time-out must be zero.', 'H76.2');
+                if (/^[1-9]\d*/.test(element.getAttribute('content').toLowerCase()) === false) {
+                    HTMLCS.addMessage(HTMLCS.ERROR, element, 'When using meta refresh to periodically refresh pages or cause a client-side redirect, the time-out must be zero.', 'F41.2');
                 }
             }//end if
         }//end if
