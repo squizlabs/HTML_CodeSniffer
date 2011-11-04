84feb6b844307490bfbf38db194871c26c5c93a7
Luke Wright <lwright@squiz.com.au>
2011-11-04 15:18:27 +1100

    Syntax error; inverted logic fix.

--- Standards/WCAG2AAA/Sniffs/Principle2/Guideline2_2/2_2_1.js
+++ Standards/WCAG2AAA/Sniffs/Principle2/Guideline2_2/2_2_1.js
@@ -12,8 +12,8 @@ var HTMLCS_WCAG2AAA_Sniffs_Principle2_Guideline2_2_2_2_1 = {
         // same page (no URL content), which is covered by non-adjustable timeouts
         // in criterion 2.2.1.
         if (element.hasAttribute('http-equiv') === true) {
-            if ((String(element.getAttribute('http-equiv'))).toLowerCase() === 'refresh')) {
-                if (/^[1-9]\d*/.test(element.getAttribute('content').toLowerCase()) === false) {
+            if ((String(element.getAttribute('http-equiv'))).toLowerCase() === 'refresh') {
+                if (/^[1-9]\d*/.test(element.getAttribute('content').toLowerCase()) === true) {
                     HTMLCS.addMessage(HTMLCS.ERROR, element, 'When using meta refresh to periodically refresh pages or cause a client-side redirect, the time-out must be zero.', 'F41.2');
                 }
             }//end if
