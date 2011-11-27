a0857f8a7b0b41841db113e854637e73895f017c
Luke Wright <lwright@squiz.com.au>
2011-11-27 18:44:02 +1100

    Beginnings of test for using MAP elements to group links (H50).

--- Standards/WCAG2AAA/Sniffs/Principle2/Guideline2_4/2_4_1.js
+++ Standards/WCAG2AAA/Sniffs/Principle2/Guideline2_4/2_4_1.js
@@ -1,11 +1,42 @@
 var HTMLCS_WCAG2AAA_Sniffs_Principle2_Guideline2_4_2_4_1 = {
     register: function()
     {
-        return ['iframe'];
+        return [
+            'iframe',
+            'p',
+            'div',
+            'ul',
+            'ol',
+        ];
 
     },
 
     process: function(element)
+    {
+        var nodeName = element.nodeName.toLowerCase();
+
+        switch (nodeName) {
+            case 'iframe':
+                this.testIframeTitle(element);
+            break;
+
+            case 'p':
+            case 'div':
+            case 'ul':
+            case 'ol':
+                this.testMapElement(element);
+            break;
+        }
+    },
+
+    /**
+     * Test for the presence of title attributes on the iframe element.
+     *
+     * @param {DOMNode} element The element to test.
+     *
+     * @returns void
+     */
+    testIframeTitle: function(element)
     {
         var nodeName = element.nodeName.toLowerCase();
 
@@ -21,6 +52,33 @@ var HTMLCS_WCAG2AAA_Sniffs_Principle2_Guideline2_4_2_4_1 = {
                 HTMLCS.addMessage(HTMLCS.ERROR, element, 'Check this element for the presence of a title attribute that identifies the frame.', 'H64.1');
             } else {
                 HTMLCS.addMessage(HTMLCS.NOTICE, element, 'Check that the title attribute of this element contains text that identifies the frame.', 'H64.2');
+            }
+        }//end if
+    },
+
+    /**
+     * Test for the presence of the map element for grouping links.
+     *
+     * @param {DOMNode} element The element to test.
+     *
+     * @returns void
+     */
+    testMapElement: function(element)
+    {
+        var links = element.querySelectorAll('a');
+        if (links.length >= 2) {
+            // Going to throw a warning here, mainly because we cannot easily tell
+            // whether it is just a paragraph with multiple links, or a navigation
+            // structure.
+            var mapFound = false;
+            var parent   = element.parentNode;
+            while ((parent !== null) && (parent.nodeName.toLowerCase() !== 'map')) {
+                parent = parent.parentNode;
+            }
+
+            if (parent === null) {
+                // Found no map element.
+                HTMLCS.addMessage(HTMLCS.WARNING, element, 'Check that links that should be grouped into logical sets (such as a navigation bar or main menu) are grouped using map elements, so they can be bypassed.', 'H50');
             }
         }//end if
     }
