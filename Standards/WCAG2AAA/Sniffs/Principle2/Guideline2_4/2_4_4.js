3b8650f1ecac51b9a9f78944b0843e3af0dca252
Luke Wright <lwright@squiz.com.au>
2011-11-27 22:57:22 +1100

    Add title attribute test to 2.4.4 (H33).
    This is another "in context" notice. Only one notice will be thrown per link: if there is a title attribute, one notice will point to H77-81 AND H33 at once, if not, just to H77-81.

--- Standards/WCAG2AAA/Sniffs/Principle2/Guideline2_4/2_4_4.js
+++ Standards/WCAG2AAA/Sniffs/Principle2/Guideline2_4/2_4_4.js
@@ -7,7 +7,11 @@ var HTMLCS_WCAG2AAA_Sniffs_Principle2_Guideline2_4_2_4_4 = {
 
     process: function(element)
     {
-        HTMLCS.addMessage(HTMLCS.NOTICE, element, 'Check that the link text combined with programmatically determined link context identifies the purpose of the link.', 'H77-81');
+        if (element.hasAttribute('title') === true) {
+            HTMLCS.addMessage(HTMLCS.NOTICE, element, 'Check that the link text combined with programmatically determined link context, or its title attribute, identifies the purpose of the link.', 'H77-81H33');
+        } else {
+            HTMLCS.addMessage(HTMLCS.NOTICE, element, 'Check that the link text combined with programmatically determined link context identifies the purpose of the link.', 'H77-81');
+        }
 
     }
 };
