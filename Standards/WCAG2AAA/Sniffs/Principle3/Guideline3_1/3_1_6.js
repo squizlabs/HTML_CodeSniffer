4bd35da1e7d6c8f99c1c48c9b053c1d9cc959906
Luke Wright <lwright@squiz.com.au>
2011-12-08 13:49:52 +1100

    Add SC 3.1.6, checking of ruby elements (H62).
    Ruby is not used widely (as it was only introduced in XHTML 1.1) however HTML5 allows it. Browser support is only for IE and Chrome/Safari though - Firefox requires an add-on for support - which makes it doubly-important that rp tags exist for other browsers.

---
+++ Standards/WCAG2AAA/Sniffs/Principle3/Guideline3_1/3_1_6.js
@@ -0,0 +1,31 @@
+var HTMLCS_WCAG2AAA_Sniffs_Principle3_Guideline3_1_3_1_6 = {
+    register: function()
+    {
+        return ['ruby'];
+
+    },
+
+    process: function(element)
+    {
+        // Generic message for changes in language.
+        var rb = element.querySelectorAll('rb');
+        var rt = element.querySelectorAll('rt');
+        if (rt.length === 0) {
+            // Vary the message depending on whether an rb element exists. If it doesn't,
+            // the presumption is that we are using HTML5 that uses the body of the ruby
+            // element for the same purpose (otherwise, assume XHTML 1.1 with rb element).
+            if (rb.length === 0) {
+                HTMLCS.addMessage(HTMLCS.ERROR, top, 'Check that a rt element contains pronunciation information for each run of text defined by the body of the ruby element.', 'H62.1');
+            } else {
+                HTMLCS.addMessage(HTMLCS.ERROR, top, 'Check that a rt element contains pronunciation information for each run of text defined by the rb element.', 'H62.1');
+            }
+        }
+
+        var rp = element.querySelectorAll('rp');
+        if (rp.length === 0) {
+            // No "ruby parentheses" tags for those user agents that don't support
+            // ruby at all.
+            HTMLCS.addMessage(HTMLCS.ERROR, top, 'If simple Ruby markup is used, check that the rp element is present to indicate to user agents that do not support Ruby annotations that the text in the rt element provides the pronunciation information.', 'H62.2');
+        }
+    }
+};
