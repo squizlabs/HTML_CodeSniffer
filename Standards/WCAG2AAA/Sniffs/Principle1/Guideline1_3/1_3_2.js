01b52b1761c824fc212636561b06bc21d7c8cf34
Luke Wright <lwright@squiz.com.au>
2011-12-09 17:10:00 +1100

    Add generic manual check messages for SC 1.3.2, 1.3.3.
    1.3.2 (Meaningful Sequence) is that items should display properly and accessibly when linearised (such as when stylesheets are removed). The generic technique is emitted but certain techniques (mainly CSS-based) are also available.
    1.3.3 (Sensory Characteristics) is simply a reminder that users should not refer to items in the content using size, shape, sound, location, etc, alone (eg. use the name or label as well).

---
+++ Standards/WCAG2AAA/Sniffs/Principle1/Guideline1_3/1_3_2.js
@@ -0,0 +1,15 @@
+var HTMLCS_WCAG2AAA_Sniffs_Principle1_Guideline1_3_1_3_2 = {
+    _labelNames: null,
+
+    register: function()
+    {
+        return ['_top'];
+
+    },
+
+    process: function(element, top)
+    {
+        HTMLCS.addMessage(HTMLCS.NOTICE, top, 'Check that the content is ordered in a meaningful sequence when linearised, such as when style sheets are disabled.', 'G57');
+
+    }
+};
---
+++ Standards/WCAG2AAA/Sniffs/Principle1/Guideline1_3/1_3_3.js
@@ -0,0 +1,15 @@
+var HTMLCS_WCAG2AAA_Sniffs_Principle1_Guideline1_3_1_3_3 = {
+    _labelNames: null,
+
+    register: function()
+    {
+        return ['_top'];
+
+    },
+
+    process: function(element, top)
+    {
+        HTMLCS.addMessage(HTMLCS.NOTICE, top, 'Where instructions are provided for understanding the content, do not rely on sensory characteristics alone (such as shape, size or location) to describe objects.', 'G96');
+
+    }
+};
