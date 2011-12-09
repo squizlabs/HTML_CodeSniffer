df376a3aa5204b7408d474d74d2e4713997fdf58
Luke Wright <lwright@squiz.com.au>
2011-12-09 17:38:07 +1100

    General notice for SC 1.4.1: Use of Colour.

---
+++ Standards/WCAG2AAA/Sniffs/Principle1/Guideline1_4/1_4_1.js
@@ -0,0 +1,15 @@
+var HTMLCS_WCAG2AAA_Sniffs_Principle1_Guideline1_4_1_4_1 = {
+    _labelNames: null,
+
+    register: function()
+    {
+        return ['_top'];
+
+    },
+
+    process: function(element, top)
+    {
+        HTMLCS.addMessage(HTMLCS.NOTICE, top, 'Check that any information conveyed using colour alone is also available in text, or through other visual cues.', 'G14G182');
+
+    }
+};
