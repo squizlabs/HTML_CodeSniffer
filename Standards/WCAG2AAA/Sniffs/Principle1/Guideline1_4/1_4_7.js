d147c6a7519100b0ecbc292d1907cc1ac5873799
Luke Wright <lwright@squiz.com.au>
2011-12-16 21:47:09 +1100

    1.4.7 mentions audio-only content, so video tag is not applicable.

--- Standards/WCAG2AAA/Sniffs/Principle1/Guideline1_4/1_4_7.js
+++ Standards/WCAG2AAA/Sniffs/Principle1/Guideline1_4/1_4_7.js
@@ -8,7 +8,7 @@ var HTMLCS_WCAG2AAA_Sniffs_Principle1_Guideline1_4_1_4_7 = {
     process: function(element, top)
     {
         // Check for elements that could potentially contain audio.
-        var mediaObj = top.querySelector('object, embed, applet, bgsound, audio, video');
+        var mediaObj = top.querySelector('object, embed, applet, bgsound, audio');
 
         if (mediaObj !== null) {
             HTMLCS.addMessage(HTMLCS.NOTICE, top, 'For pre-recorded audio-only content that is primarily speech (such as narration), any background sounds should be muteable, or be at least 20 dB (or about 4 times) quieter than the speech.', 'G56');
