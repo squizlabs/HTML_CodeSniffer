e0b37c71db381cd87581228fb2cabb39c9ec1345
Luke Wright <lwright@squiz.com.au>
2011-12-19 22:10:14 +1100

    Add single-A ruleset; remove 3.2.5 from AA ruleset.

--- Standards/WCAG2A/ruleset.js
+++ Standards/WCAG2A/ruleset.js
@@ -1,6 +1,6 @@
-var HTMLCS_WCAG2AA = {
-    name: 'WCAG2AA',
-    description: 'Web Content Accessibility Guidelines (WCAG) 2.0 AA',
+var HTMLCS_WCAG2A = {
+    name: 'WCAG2A',
+    description: 'Web Content Accessibility Guidelines (WCAG) 2.0 A',
     sniffs: [
         {
             standard: 'WCAG2AAA',
@@ -8,16 +8,12 @@ var HTMLCS_WCAG2AA = {
                'Principle1.Guideline1_1.1_1_1',
                'Principle1.Guideline1_2.1_2_1',
                'Principle1.Guideline1_2.1_2_2',
-               'Principle1.Guideline1_2.1_2_4',
-               'Principle1.Guideline1_2.1_2_5',
+               'Principle1.Guideline1_2.1_2_3',
                'Principle1.Guideline1_3.1_3_1',
                'Principle1.Guideline1_3.1_3_2',
                'Principle1.Guideline1_3.1_3_3',
                'Principle1.Guideline1_4.1_4_1',
                'Principle1.Guideline1_4.1_4_2',
-               'Principle1.Guideline1_4.1_4_3',
-               'Principle1.Guideline1_4.1_4_4',
-               'Principle1.Guideline1_4.1_4_5',
                'Principle2.Guideline2_1.2_1_1',
                'Principle2.Guideline2_1.2_1_2',
                'Principle2.Guideline2_2.2_2_1',
@@ -27,19 +23,11 @@ var HTMLCS_WCAG2AA = {
                'Principle2.Guideline2_4.2_4_2',
                'Principle2.Guideline2_4.2_4_3',
                'Principle2.Guideline2_4.2_4_4',
-               'Principle2.Guideline2_4.2_4_5',
-               'Principle2.Guideline2_4.2_4_6',
-               'Principle2.Guideline2_4.2_4_7',
                'Principle3.Guideline3_1.3_1_1',
-               'Principle3.Guideline3_1.3_1_2',
                'Principle3.Guideline3_2.3_2_1',
                'Principle3.Guideline3_2.3_2_2',
-               'Principle3.Guideline3_2.3_2_3',
-               'Principle3.Guideline3_2.3_2_4',
                'Principle3.Guideline3_3.3_3_1',
                'Principle3.Guideline3_3.3_3_2',
-               'Principle3.Guideline3_3.3_3_3',
-               'Principle3.Guideline3_3.3_3_4',
                'Principle4.Guideline4_1.4_1_1',
                'Principle4.Guideline4_1.4_1_2'
             ]
