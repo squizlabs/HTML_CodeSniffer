ce615935c2aaaaafe6243caed3cb3646539609ef
Luke Wright <lwright@squiz.com.au>
2012-01-10 19:19:18 +1100

    Fix JS error in 1.2.1, forgot to place an index on an array reference.

--- Standards/WCAG2AAA/Sniffs/Principle1/Guideline1_2/1_2_1.js
+++ Standards/WCAG2AAA/Sniffs/Principle1/Guideline1_2/1_2_1.js
@@ -25,7 +25,7 @@ var HTMLCS_WCAG2AAA_Sniffs_Principle1_Guideline1_2_1_2_1 = {
         var mediaObjs = top.querySelectorAll('object, embed, applet, bgsound, audio, video');
 
         for (var i = 0; i < mediaObjs.length; i++) {
-            var nodeName = mediaObjs.nodeName.toLowerCase();
+            var nodeName = mediaObjs[i].nodeName.toLowerCase();
 
             if (nodeName !== 'video') {
                 HTMLCS.addMessage(HTMLCS.NOTICE, mediaObjs[i], 'If this embedded object contains pre-recorded audio only, and is not provided as an alternative for text content, check that an alternative text version is available.', 'G158');
