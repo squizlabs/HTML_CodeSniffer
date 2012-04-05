58799f43b444947b37b3b64c4a0784053dc2c4bd
Luke Wright <lwright@squiz.com.au>
2012-04-05 12:53:28 +1000

    SC 2.2.5: typo.

--- Standards/WCAG2AAA/Sniffs/Principle2/Guideline2_2/2_2_5.js
+++ Standards/WCAG2AAA/Sniffs/Principle2/Guideline2_2/2_2_5.js
@@ -21,7 +21,7 @@ var HTMLCS_WCAG2AAA_Sniffs_Principle2_Guideline2_2_2_2_5 = {
      */
     process: function(element, top)
     {
-        HTMLCS.addMessage(HTMLCS.NOTICE, element, 'If this Web page is part of a set of Web pages with an inactivity time limit, check that an authenticated user can continue the activity without loss of data after re-authenticating..', 'G105,G181');
+        HTMLCS.addMessage(HTMLCS.NOTICE, element, 'If this Web page is part of a set of Web pages with an inactivity time limit, check that an authenticated user can continue the activity without loss of data after re-authenticating.', 'G105,G181');
 
     }
 };
