203031f5a224d1fee5ea95233e9fef9000614b10
Michael Fairchild <mfairchild365@gmail.com>
2014-07-09 09:17:06 -0500

    Assume a white background if no solid colors were found

--- HTMLCS.js
+++ HTMLCS.js
@@ -864,6 +864,12 @@ var HTMLCS = new function()
             //Find all the background with transparancy until we get to a solid colour
             while (solidFound == false) {
                 if ((!parent) || (!parent.ownerDocument)) {
+                    //No parent was found, assume a solid white background.
+                    backgrounds.push({
+                        red: 1,
+                        green: 1,
+                        blue: 1
+                    });
                     break;
                 }
                 
