e5142f9a30e7e7c9f84f0cd73580550b22b56093
Anthony Barnes <abarnes@squiz.com.au>
2016-08-25 08:09:30 +1000

    Allowed unit tests to output individual test results to the console

--- Contrib/Testing/run_tests.sh
+++ Contrib/Testing/run_tests.sh
@@ -11,18 +11,18 @@ for FILE in $FILES; do
 	REMAIN=`expr $NUMTESTS % 40`
 	if [[ "x$REMAIN" == "x0" ]]; then
 		if [[ "x$NUMTESTS" != "x0" ]]; then
-			echo ''
+			echo ' '
 		fi
 	fi
 
 	NUMTESTS=`expr $NUMTESTS + 1`
-	phantomjs HTMLCS_Test.js $FILE > /dev/null
+	phantomjs HTMLCS_Test.js $FILE
 	if [[ "x$?" == "x0" ]]; then
 		NUMPASS=`expr $NUMPASS + 1`
-		echo -n '.'
+		echo -n "PASS $FILE"
 	else
 		NUMFAIL=`expr $NUMFAIL + 1`
-		echo -n 'F'
+		echo -n "FAIL $FILE"
 	fi
 done
 
