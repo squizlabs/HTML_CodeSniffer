1e53a517fcca6f8ff50f8bf9b84b0604e380e7f9
Juga Paazmaya <paazmaya@yahoo.com>
2018-11-15 16:25:51 +0200

    Going back to ES5

--- Contrib/PhantomJS/test.js
+++ Contrib/PhantomJS/test.js
@@ -8,24 +8,24 @@ process.stdin.on('data', function(chunk) {
 });
 
 function toHTML(obj) {
-    return `<tr>
-        <td>${obj.code}</td>
-        <td>${obj.msg}</td>
-    </tr>`;
+    return '<tr>' +
+        '<td>${obj.code}</td>' +
+        '<td>${obj.msg}</td>' +
+    '</tr>';
 }
 
 process.stdin.on('end', function() {
     var issues = JSON.parse(data);
-    var html = `<table>
-        <thead>
-            <tr>
-                <th>Code</th>
-                <th>Message<th>
-            </tr>
-        </thead>
-        <tbody>
-            ${issues.map(toHTML).join('')}
-        </tbody>
-    </table>`;
+    var html = '<table>' +
+        '<thead>' +
+          '  <tr>' +
+                '<th>Code</th>' +
+                '<th>Message<th>' +
+            '</tr>' +
+        '</thead>' +
+        '<tbody>' +
+            issues.map(toHTML).join('') +
+        '</tbody>' +
+    '</table>';
     console.log(html);
 });
